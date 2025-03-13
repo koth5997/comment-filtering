53340c63cdf8ef8083d8291653e23d9203287a8e
고태희 <127705834+koth5997@users.noreply.github.com>
2025-03-13 17:30:22 +0900

    Update app.py
    주석 이랑 코멘트 수정

--- comment-filtering/backend/app.py
+++ comment-filtering/backend/app.py
@@ -5,24 +5,23 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # 필터링할 금칙어 리스트
-BAD_WORDS = ["이건 정상 댓글", "씨발 뭐야", "1찍", "2찍","공산","이재명","윤석열"]
+BAD_WORDS = ["씨발", "병신", "1찍", "2찍","공산","이재명","윤석열"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 모든 도메인 허용
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# 요청 받을 데이터 모델
+
 class CommentRequest(BaseModel):
     comments: list[str]  # 댓글 리스트 받기
 
-# 기본 루트 엔드포인트 추가
+
 @app.get("/")
 def home():
     return {"message": "Comment filtering API is running!"}
 
-# 필터링 API (POST 요청)
 @app.post("/filter")
 def filter_comments(request: CommentRequest):
     filtered = [comment for comment in request.comments if any(word in comment for word in BAD_WORDS)]
